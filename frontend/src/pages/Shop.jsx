797fa17741d2fc2d273eae7bea55d2949a6a4eb1
tech-codec <varuslimogne@gmail.com>
2025-03-04 20:47:06 +0100

    ajout du loader

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -12,8 +12,9 @@ export default function Shop() {
     <div className='px-5p md:px-10p  mt-5'>
       {
         loading ?  (
-          <div className='px-3 md:px-8 flex items-center flex-col justify-center h-screen'>
+          <div className='px-3  md:px-8 flex items-center flex-col justify-center h-450px md:h-screen'>
             <LoadingLoader />
+            <p className='text-xl text-gray-500 text-center mt-3'>Patientez quelques minutes le temps que les données chargent</p>
           </div>
         ) :(
           <>
