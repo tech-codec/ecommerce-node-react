912712a04d4d55a7482df444eea34baf3702aa77
tech-codec <varuslimogne@gmail.com>
2025-03-10 23:43:56 +0100

    Réglade de la vue l'activation de compte

--- admin/src/pages/ActivateAccount.jsx
+++ admin/src/pages/ActivateAccount.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import axios from '../utils/axiosConfig';
-
+import { useTheme } from '../context/ThemeContext';
+import ThemeToggleButton from '../components/ThemeToggleButton';
 const ActivateAccount = () => {
   const { token } = useParams();
   const navigate = useNavigate();
   const [message, setMessage] = useState('');
+  const { theme } = useTheme();
  
 
   useEffect(() => {
@@ -28,12 +30,19 @@ const ActivateAccount = () => {
   }, [token, navigate]);
 
   return (
-    <div className="flex items-center justify-center my-5p">
+
+    <div className={`w-full flex flex-col px-4 items-center h-screen justify-center ${theme === 'dark' ? 'bg-gray-900' : 'bg-gray-200'}`}>
+     <div className="flex justify-end mb-4">
+        <ThemeToggleButton />
+      </div>
+     <div className="flex items-center justify-center my-5p">
       <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
         <h1 className="text-2xl font-bold mb-4">Activation de compte</h1>
         <p>{message}</p>
       </div>
     </div>
+    </div>
+    
   );
 };
 
